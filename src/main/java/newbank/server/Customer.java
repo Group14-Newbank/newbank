e7b22d39d22033d3d3866da9d2b1921374a3f3de
Jules Konan <juliano60@hotmail.co.uk>
2021-03-14 08:08:12 +0000

    25 - password implementation update

--- src/main/java/newbank/server/Customer.java
+++ src/main/java/newbank/server/Customer.java
@@ -3,36 +3,33 @@ package newbank.server;
 import java.util.ArrayList;
 
 public class Customer {
-	private String username;
-	private String password;
-	private ArrayList<Account> accounts;
-	
-	public Customer( final String username, final String password ) {
-		this.username = username;
-		this.password = password;
-	}
-	
-	public Customer() {
-		accounts = new ArrayList<>();
-	}
-	
-	public String accountsToString() {
-		String s = "";
-		for(Account a : accounts) {
-			s += a.toString();
-		}
-		return s;
-	}
+  private String username;
+  private String password;
+  private ArrayList<Account> accounts;
 
-	public void addAccount(Account account) {
-		accounts.add(account);		
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
+  public Customer(final String username, final String password) {
+    this.username = username;
+    this.password = password;
+    accounts = new ArrayList<>();
+  }
+
+  public String accountsToString() {
+    String s = "";
+    for (Account a : accounts) {
+      s += a.toString();
+    }
+    return s;
+  }
+
+  public void addAccount(Account account) {
+    accounts.add(account);
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
 }
