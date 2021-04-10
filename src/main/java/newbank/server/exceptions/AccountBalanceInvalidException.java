e603e038e695eee001e310f12bf76ebbdb67b2e2
Jules Konan <juliano60@hotmail.co.uk>
2021-04-10 12:13:48 +0100

    81 - Add default current account feature

---
+++ src/main/java/newbank/server/exceptions/AccountBalanceInvalidException.java
@@ -0,0 +1,5 @@
+package newbank.server.exceptions;
+
+public class AccountBalanceInvalidException extends Exception {
+  private static final long serialVersionUID = 8234423235864929104L;
+}
---
+++ src/main/java/newbank/server/exceptions/AccountTypeInvalidException.java
@@ -0,0 +1,6 @@
+package newbank.server.exceptions;
+
+public class AccountTypeInvalidException extends Exception {
+  private static final long serialVersionUID = 7454103697449085723L;
+	
+}
